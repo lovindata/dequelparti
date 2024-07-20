e71706c413c0f5203033e85bd96e5f9d2907169c
lovindata <ilovedata.jjia@gmail.com>
2024-07-20 16:46:42 +0200

    DONE: Fix 'Sorted vocabulary'

--- dataeng/src/modules/vocabulary_prep/vocabulary_vo.py
+++ dataeng/src/modules/vocabulary_prep/vocabulary_vo.py
@@ -5,3 +5,6 @@ from typing import List, Mapping
 @dataclass
 class VocabularyVo:
     root: Mapping[str, List[float]]
+
+    def __post_init__(self) -> None:
+        self.root = dict(sorted(self.root.items()))
