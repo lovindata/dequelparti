bfd53fb25706628eef9e331366efd0273c888674
lovindata <ilovedata.jjia@gmail.com>
2024-09-05 07:31:54 +0200

    DONE: Add 'NavBar backdrop-blur effect & sticky top'

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,32 +1,26 @@
 import nextConfig from "@/next.config.mjs";
 import { DarkmodeToggle } from "@/src/components/navbar/darkmode-toggle";
-import { cn } from "@/src/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Navbar({ className }: Props) {
+export function Navbar() {
   return (
-    <div
-      className={cn(
-        "m-2 flex items-center justify-between rounded-2xl border bg-card px-3 py-2 md:m-4",
-        className,
-      )}
-    >
-      <Link
-        className="flex items-center space-x-2 font-semibold text-muted-foreground hover:text-current"
-        href="/"
-      >
-        <Image
-          src={`${nextConfig.basePath}/imgs/logo.webp`}
-          alt={"DeQuelParti"}
-          width={62}
-          height={32}
-        />
-        <span>DeQuelParti</span>
-      </Link>
-      <DarkmodeToggle />
+    <div className="sticky top-0 z-50">
+      <div className="m-2 flex items-center justify-between rounded-2xl border px-3 py-2 shadow backdrop-blur md:m-4">
+        <Link
+          className="flex items-center space-x-2 font-semibold text-muted-foreground hover:text-current"
+          href="/"
+        >
+          <Image
+            src={`${nextConfig.basePath}/imgs/logo.webp`}
+            alt="DeQuelParti"
+            width={62}
+            height={32}
+          />
+          <span>DeQuelParti</span>
+        </Link>
+        <DarkmodeToggle />
+      </div>
     </div>
   );
 }
